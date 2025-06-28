ef87f210fb90726b7cc5cb899c6e98cfec525cff
Gino <akapb8641@gmail.com>
2025-06-28 12:35:59 +0800

    新增 Designers.jsx，修正部署錯誤

---
+++ frontend/src/components/Designers.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Designers() {
+  return (
+    <div style={{padding: '2rem'}}>
+      <h2>設計師管理</h2>
+      <p>這裡是設計師管理頁面，功能待開發。</p>
+    </div>
+  );
+}
+
+export default Designers; 
