15f33dfdd3a1bb804a8fe8a34a627d8d195043f2
Gino <akapb8641@gmail.com>
2025-06-28 12:01:03 +0800

    新增 Customers.jsx，修正部署錯誤

---
+++ frontend/src/components/Customers.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Customers() {
+  return (
+    <div style={{padding: '2rem'}}>
+      <h2>客戶管理</h2>
+      <p>這裡是客戶管理頁面，功能待開發。</p>
+    </div>
+  );
+}
+
+export default Customers; 
